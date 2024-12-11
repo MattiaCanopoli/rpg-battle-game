5cbfa986ed03e0ce0f5e4e02a3dd30b6187a04cf
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-12-11 15:26:17 +0100

    rm: application.properties; add: CharacterClassRepo

---
+++ src/main/java/com/rpgbattle/com/repository/CharacterClassRepo.java
@@ -0,0 +1,9 @@
+package com.rpgbattle.com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rpgbattle.com.model.CharacterClass;
+
+public interface CharacterClassRepo  extends JpaRepository<CharacterClass,Integer>{
+
+}
