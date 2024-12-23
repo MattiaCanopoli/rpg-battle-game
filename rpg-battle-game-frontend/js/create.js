1c2e3e869a352e37b78754c558505b935ae9cb85
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-12-24 00:00:56 +0100

    fix: minor changes

--- rpg-battle-game-frontend/js/create.js
+++ rpg-battle-game-frontend/js/create.js
@@ -1,19 +1,13 @@
-'use strict'
-import {
-    rollChar,
-} from "./common.js"
+"use strict";
+import { rollChar } from "./common.js";
 
+const btn = document.getElementById("testBtn");
 
-const btn = document.getElementById("testBtn")
-
-let charRolls = []
+let charRolls = [];
 
 btn.addEventListener("click", function () {
-    charRolls = [];
-    for (let i = 0; i < 6; i++) {
-        charRolls.push(rollChar())
-    }
-})
-
-
-
+  charRolls = [];
+  for (let i = 0; i < 6; i++) {
+    charRolls.push(rollChar());
+  }
+});
