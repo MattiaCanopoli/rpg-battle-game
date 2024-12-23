9edbf73c4b81a99c8948f64e8a2bcbe77ba732ce
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-12-24 00:00:43 +0100

    fix: rollDice value; add: sumRolls function

--- rpg-battle-game-frontend/js/common.js
+++ rpg-battle-game-frontend/js/common.js
@@ -1,38 +1,41 @@
-'use strict'
+"use strict";
 
 export function rollDice(times, value) {
-    let rolls = [];
-    for (let i = 0; i < times; i++) {
-        let roll = Math.ceil(Math.random() * value)
-        rolls.push(roll)
-    }
+  let rolls = [];
+  for (let i = 0; i < times; i++) {
+    let roll = Math.floor(Math.random() * (value - 1) + 1);
+    rolls.push(roll);
+  }
+  return rolls;
+}
 
-    let sum = 0;
-    for (let i = 0; i < rolls.length; i++) {
-        sum += rolls[i];
-    }
-    return sum
+export function sumRolls(rollsArray) {
+  let sum = 0;
+  for (let i = 0; i < rollsArray.length; i++) {
+    sum += rollsArray[i];
+  }
+  return sum;
 }
 
 export function rollChar() {
-    let rolls = [];
-    //add 4 rolls to rolls array
-    for (let i = 0; i < 4; i++) {
-        rolls.push(Math.ceil(Math.random() * 7))
-    }
-    //find min roll
-    let minRoll = Math.min.apply(null, rolls);
-    //remove min roll from array
-    rolls.splice(rolls.indexOf(minRoll), 1);
-    //sums all the values
-    let sum = 0;
-    for (let i = 0; i < rolls.length; i++) {
-        sum += rolls[i];
-    }
-    return sum;
+  let rolls = [];
+  //add 4 rolls to rolls array
+  for (let i = 0; i < 4; i++) {
+    rolls.push(Math.ceil(Math.random() * 7));
+  }
+  //find min roll
+  let minRoll = Math.min.apply(null, rolls);
+  //remove min roll from array
+  rolls.splice(rolls.indexOf(minRoll), 1);
+  //sums all the values
+  let sum = 0;
+  for (let i = 0; i < rolls.length; i++) {
+    sum += rolls[i];
+  }
+  return sum;
 }
 
 export function logToTextarea(textarea, message) {
-    textarea.value += message + "\n";
-    textarea.scrollTop = textarea.scrollHeight
+  textarea.value += message + "\n";
+  textarea.scrollTop = textarea.scrollHeight;
 }
