db2f7e6b0830247759a248499ab994cc6e29b109
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-12-24 00:00:01 +0100

    fix: hit value in attack; messages in attack

--- rpg-battle-game-frontend/js/battleground.js
+++ rpg-battle-game-frontend/js/battleground.js
@@ -4,20 +4,17 @@ creare una tabella ogni volta che si incontra un nuovo array
 popolare la tabella inserendo i valori in vari td -OK
 trovare un modo per ordinare la tabella*/
 
-'use strict'
+"use strict";
 
-import {
-  rollDice,
-  logToTextarea
-} from "./common.js"
+import { rollDice, logToTextarea, sumRolls } from "./common.js";
 
 // FUNCTIONS
 
 /*adds a row with two cells to a table*/
 function createTableRows(cellOneText, cellTwoText, tableName) {
   const table = document.getElementById(tableName);
-  const row = table.insertRow()
-  row.classList.add("text-capitalize")
+  const row = table.insertRow();
+  row.classList.add("text-capitalize");
   const celllOne = row.insertCell(0);
   const cellTwo = row.insertCell(1);
   celllOne.innerText = cellOneText;
@@ -28,56 +25,70 @@ function createTableRows(cellOneText, cellTwoText, tableName) {
 async function getData(url, tableId) {
   const response = await fetch(url);
   const body = await response.json();
-  console.log(body)
+  //console.log(body)
   for (let i = 0; i < body.length; i++) {
     body.forEach(function (element) {
       const keys = Object.getOwnPropertyNames(element);
       keys.forEach(function (key) {
-        createTableRows(key, body[i][key], tableId)
-      })
-    })
+        createTableRows(key, body[i][key], tableId);
+      });
+    });
   }
 }
 
 /*adds a CSS class to an HTML element.
 CSS is removed after timeout time */
 function addRemoveClass(elementId, cssAnimation, timeout) {
-  const element = document.getElementById(elementId)
-  element.classList.add(cssAnimation)
+  const element = document.getElementById(elementId);
+  element.classList.add(cssAnimation);
   setTimeout(function () {
-    element.classList.remove(cssAnimation)
-  }, timeout)
+    element.classList.remove(cssAnimation);
+  }, timeout);
 }
 
-function attack(textarea, monsterDef, diceNumber, diceValue, hitModifier, critValue, critMultiplier) {
-  const hit = rollDice(1, 20) + hitModifier;
+function attack(
+  textarea,
+  monsterDef,
+  diceNumber,
+  diceValue,
+  hitModifier,
+  critValue,
+  critMultiplier
+) {
+  const hit = sumRolls(rollDice(1, 20)) + hitModifier;
   let message;
   if (hit >= monsterDef) {
-    let damage = rollDice(diceNumber, diceValue)
+    let damageRolls = rollDice(diceNumber, diceValue);
+    let damage = sumRolls(damageRolls);
     if (hit >= critValue) {
       damage = damage * critMultiplier;
-      message = `Critico!\n    hai colpito con ${hit}\n    il nemico ha subito ${damage} danni`
+      message = `Critico!\n    hai colpito con ${hit}\n    il nemico ha subito ${damage} danni (rolls: ${damageRolls} x ${critMultiplier})`;
     } else {
-      message = `hai colpito con ${hit}\n    il nemico ha subito ${damage} danni`
+      message = `hai colpito con ${hit}\n    il nemico ha subito ${damage} danni (rolls: ${damageRolls})`;
     }
   } else {
-    message = `hai mancato il colpo!`
+    message = `hai mancato il colpo! (hit: ${hit})`;
   }
 
-  logToTextarea(textarea, message)
+  logToTextarea(textarea, message);
 }
 
-const url = "http://localhost:8080/api/"
-const charKeys = ['race.name', 'characterClass.name', 'lifePoints', 'level', 'money']
-const animationBtn = document.getElementById("btn")
-const logArea = document.getElementById("log")
+const url = "http://localhost:8080/api/";
+const charKeys = [
+  "race.name",
+  "characterClass.name",
+  "lifePoints",
+  "level",
+  "money",
+];
+const animationBtn = document.getElementById("btn");
+const logArea = document.getElementById("log");
 
-getData(url + "creatures", "monster-table")
-getData(url + "creatures", "player-table")
+getData(url + "creatures", "monster-table");
+getData(url + "creatures", "player-table");
 
 animationBtn.addEventListener("click", function () {
-  addRemoveClass("character", 'move-left', 200)
+  addRemoveClass("character", "move-left", 200);
   //TODO: sostituire valori hardcoded con valori runtime
-  attack(logArea, 15, 2, 8, 0, 19, 2)
-
-})
+  attack(logArea, 15, 2, 8, 0, 19, 2);
+});
